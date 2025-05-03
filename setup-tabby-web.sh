26f6150b1d2aa15c33c3385243a8552bd13dd88e
willjasen <brandon@willjasen.com>
2025-05-02 23:57:29 -0400

    switch back to which

--- setup-tabby-web.sh
+++ setup-tabby-web.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Check if jq is installed
-if ! command -v jq &> /dev/null; then
-    echo "\033[33mjq is not installed. Installing jq...\033[0m"
+if [ ! -x "$(which jq)" ]; then
+    echo -e "\033[33mjq is not installed. Installing jq...\033[0m"
     apt-get update && apt-get install -y jq
 fi
 
