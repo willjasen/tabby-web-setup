f934a07191aede7a22149307b354e2d8da7e841c
willjasen <brandon@willjasen.com>
2025-05-03 00:18:57 -0400

    use version 1.0.163

--- setup-tabby-web.sh
+++ setup-tabby-web.sh
@@ -38,13 +38,15 @@ else
 fi
 
 # Fetch the JSON file from the URL
-URL="https://registry.npmjs.org/tabby-web-container/"
-LATEST_VERSION=$(curl -s $URL | jq -r '."dist-tags".latest')
-LATEST_VERSION_TIME=$(curl -s $URL | jq -r --arg version "$LATEST_VERSION" '.time[$version]')
+# URL="https://registry.npmjs.org/tabby-web-container/"
+# LATEST_VERSION=$(curl -s $URL | jq -r '."dist-tags".latest')
+# LATEST_VERSION_TIME=$(curl -s $URL | jq -r --arg version "$LATEST_VERSION" '.time[$version]')
 
 # Print the latest version and its time
-echo "Latest version of tabby-web: $LATEST_VERSION"
-echo "Release time of latest version: $LATEST_VERSION_TIME"
+# echo "Latest version of tabby-web: $LATEST_VERSION"
+# echo "Release time of latest version: $LATEST_VERSION_TIME"
+
+LATEST_VERSION="1.0.163"
 
 # Bring up the Docker container
 docker compose -f /opt/tabby-web-setup/docker-compose.yml up -d
